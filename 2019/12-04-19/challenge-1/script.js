22affb827823b37167d55c8d80781ad07e923b8c
Timothy Beck <johndaveleet@gmail.com>
2019-12-03 22:30:38 -0700

    finished challenge 1 script

---
+++ 2019/12-04-19/challenge-1/script.js
@@ -0,0 +1,36 @@
+/**
+ * This is challenge 1 of 2019-12-04 of Advent of Code's 25-day challenge
+ *
+ *
+ * @param {number} lowerValue
+ * @param {number} higherValue
+ * @returns int
+ * @author Timothy Beck <Dev@TimothyBeck.com>
+ */
+
+function passwordFinder(lowerValue, higherValue) {
+	let incrementer = 0;
+	for(i = lowerValue; i <= higherValue; i++) {
+		if(i.toString().charAt(0) <= i.toString().charAt(1) &&
+			i.toString().charAt(1) <= i.toString().charAt(2) &&
+			i.toString().charAt(2) <= i.toString().charAt(3) &&
+		i.toString().charAt(3) <= i.toString().charAt(4) &&
+		i.toString().charAt(4) <= i.toString().charAt(5)) {
+
+			if(i.toString().charAt(0) === i.toString().charAt(1) ||
+				i.toString().charAt(2) === i.toString().charAt(1) ||
+				i.toString().charAt(2) === i.toString().charAt(3) ||
+				i.toString().charAt(4) === i.toString().charAt(3) ||
+				i.toString().charAt(4) === i.toString().charAt(5)) {
+				incrementer = incrementer + 1;
+			}
+		}
+	}
+	return incrementer;
+}
+
+
+
+let lowerValue = 136760;
+let higherValue = 595730;
+console.log(passwordFinder(lowerValue, higherValue));
