37467b32bd51aa9e7aef620bae74c64f29352d3f
Timothy Beck <johndaveleet@gmail.com>
2019-12-09 03:09:54 -0700

    modified comments to be more descriptive

--- 2019/12-09-19/challenge-2/script.js
+++ 2019/12-09-19/challenge-2/script.js
@@ -2,126 +2,113 @@
  * This is challenge 1 of 2019-12-09 of Advent of Code's 25-day challenge
  * See the readme for an explanation
  *
+ * @param arr array containing the intcode instructions
+ * @param input integer that modifies the output
  * @author Timothy Beck <Dev@TimothyBeck.com>
  */
 
-function intcode(arr, phaseSetting, input) {
-	//arr[1] = noun;
-	//arr[2] = verb;
-	let phaseCount = 0;
+
+function intcodeProgram(arr, input) {
+	//relative base is a variable modified by parameter mode: relative mode.
 	let relativeBase = 0;
 	for(let i = 0; i <= arr.length - 1; i) {
-		//console.log(i);
-		//console.log(`i = ${i} and last value = ${Number(arr[i].toString().charAt(arr[i].toString().length - 1))}`);
-		let opcodeValueOne = arr[arr[i + 1]];
-		let opcodeValueTwo = arr[arr[i + 2]];
-		let nextOpcode = i + 4;
-		let intcodeComplete = arr.length;
+		
+		//opcode parameters are parameters for the opcode Instruction,
+		let opcodeParamOne = arr[arr[i + 1]];
+		let opcodeParamTwo = arr[arr[i + 2]];
 
-		//opcode address is the last digit of the first item in each instruction
+		//Opcode Address is the last two digits of an up to 5 digit number in the first item in each instruction
 		let opcodeAddress = Number(arr[i].toString().slice(-2));
-		//these variables store the instruction parameters
-		let opParamHund = Number(arr[i].toString().charAt(arr[i].toString().length - 3));
-		let opParamThou = Number(arr[i].toString().charAt(arr[i].toString().length - 4));
-		let opParamTenThou = Number(arr[i].toString().charAt(arr[i].toString().length - 5));
+		//targets the parameter for opcodeParamOne in the opcode address; one digit before address
+		let paramModeOne = Number(arr[i].toString().charAt(arr[i].toString().length - 3));
+		//targets the parameter for opcodeParamTwo in the opcode address; two digits before address
+		let paramModeTwo = Number(arr[i].toString().charAt(arr[i].toString().length - 4));
+		//targets the parameter for opcodeParamThree in the opcode address; three digits before address
+		let paramModeThree = Number(arr[i].toString().charAt(arr[i].toString().length - 5));
 
-
-		if(opParamHund === 1) {
+		//sets parameter mode of opcode parameter one
+		if(paramModeOne === 1) {
 			//immediate mode
-			opcodeValueOne = arr[i + 1];
-		}else if(opParamHund === 2) {
+			opcodeParamOne = arr[i + 1];
+		}else if(paramModeOne === 2) {
 			//relative mode
-			opcodeValueOne = arr[relativeBase + arr[i + 1]];
+			opcodeParamOne = arr[relativeBase + arr[i + 1]];
 		}else{
 			//position mode
-			opcodeValueOne = arr[arr[i + 1]];
+			opcodeParamOne = arr[arr[i + 1]];
 		}
 
-		if(opParamThou === 1) {
+		//sets parameter mode of opcode parameter otwo
+		if(paramModeTwo === 1) {
 			//immediate mode
-			opcodeValueTwo = arr[i + 2];
-		}else if(opParamThou === 2) {
+			opcodeParamTwo = arr[i + 2];
+		}else if(paramModeTwo === 2) {
 			//relative mode
-			opcodeValueTwo = arr[relativeBase +  arr[i + 2]];
+			opcodeParamTwo = arr[relativeBase +  arr[i + 2]];
 		}else {
 			//position mode
-			opcodeValueTwo = arr[arr[i + 2]];
+			opcodeParamTwo = arr[arr[i + 2]];
 		}
 
 		//intcode logic - if opcode address is a value determining action, do said action
 		//if address is one, add designated values in the next two positions or the array
 		//apply the returned value to the position designated by the value of the 4th position in each intcode
 		if(opcodeAddress === 1) {
-			if(opParamTenThou === 2) {
-				arr[relativeBase + arr[i + 3]] = opcodeValueOne + opcodeValueTwo;
+			//opcode address 1 adds its parameters
+			if(paramModeThree === 2) {
+				arr[relativeBase + arr[i + 3]] = opcodeParamOne + opcodeParamTwo;
 			} else{
-				arr[arr[i + 3]] = opcodeValueOne + opcodeValueTwo;
+				arr[arr[i + 3]] = opcodeParamOne + opcodeParamTwo;
 			}
-			i = nextOpcode;
+			i = i + 4; //to next opcode
 		} else if(opcodeAddress === 2) {
-			if(opParamTenThou === 2) {
-				arr[relativeBase + arr[i + 3]] = opcodeValueOne * opcodeValueTwo;
+			//opcode address 2 mutliplies its parameters
+			if(paramModeThree === 2) {
+				arr[relativeBase + arr[i + 3]] = opcodeParamOne * opcodeParamTwo;
 			} else{
-				arr[arr[i + 3]] = opcodeValueOne * opcodeValueTwo;
+				arr[arr[i + 3]] = opcodeParamOne * opcodeParamTwo;
 			}
-			i = nextOpcode;
+			i = i + 4; //to next opcode
 		} else if(opcodeAddress === 99) {
 			//if address is 99, intcode program is complete
-			//console.log(`i = ${i} and arr[0] = ${arr[0]} at the end of the loop`);
-			i = intcodeComplete;
-			return `value at pos 0: ${arr[0]}`;
+			i = arr.length(); //exits the intcode program
 		} else if(opcodeAddress === 3) {
-			//outputs the stored value at the index of its parameter's value
-			if(opParamHund === 1) {
-				if(phaseCount === 0){
-					arr[i + 1] = phaseSetting;
-					phaseCount = 1;
-				}else{
+			//address 3 stores the input at its parameter
+			if(paramModeOne === 1) {
 					arr[i + 1] = input
-				}
-			} else if(opParamHund === 2) {
-				if(phaseCount === 0){
-					arr[relativeBase + arr[i + 1]] = phaseSetting;
-					phaseCount = 1;
-				}else{
+			} else if(paramModeOne === 2) {
 					arr[relativeBase + arr[i + 1]] = input
-				}
 			} else {
-				if(phaseCount === 0){
-					arr[arr[i + 1]] = phaseSetting;
-					phaseCount = 1;
-				}else{
 					arr[arr[i + 1]] = input
-				}
 			}
 			i = i + 2;
 		} else if(opcodeAddress === 4) {
-			//logs the parameter
-			if(opParamHund === 1) {
+			//address 4 returns its parameter
+			if(paramModeOne === 1) {
 				return Number(arr[i + 1]);
-			} else if(opParamHund === 2) {
+			} else if(paramModeOne === 2) {
 				return Number(arr[relativeBase +  arr[i + 1]]);
 			} else {
 				return Number(arr[arr[i + 1]]);
 			}
 		} else if(opcodeAddress === 5) {
-			//logs the parameter
-			if(opcodeValueOne !== 0) {
-				i = opcodeValueTwo;
+			//address 5 might change the instruction pointer to another location in the code
+			if(opcodeParamOne !== 0) {
+				i = opcodeParamTwo;
 			} else {
 				i = i + 3
 			}
 		} else if(opcodeAddress === 6) {
-			//logs the parameter
-			if(opcodeValueOne === 0) {
-				i = opcodeValueTwo;
+			//address 6 might change the instruction pointer to another location in the code
+			if(opcodeParamOne === 0) {
+				i = opcodeParamTwo;
 			} else {
 				i = i + 3
 			}
 		} else if(opcodeAddress === 7) {
-			//logs the parameter
-			if(opParamTenThou === 2) {
-				if(opcodeValueOne < opcodeValueTwo) {
+			//changes a value in the program to 1 or 0 depending on its parameters values
+			if(paramModeThree === 2) {
+				if(opcodeParamOne < opcodeParamTwo) {
 					arr[relativeBase +  arr[i + 3]] = 1;
 					i = i + 4;
 				} else {
@@ -129,7 +116,7 @@ function intcode(arr, phaseSetting, input) {
 					i = i + 4;
 				}
 			} else {
-				if(opcodeValueOne < opcodeValueTwo) {
+				if(opcodeParamOne < opcodeParamTwo) {
 					arr[arr[i + 3]] = 1;
 					i = i + 4;
 				} else {
@@ -138,9 +125,9 @@ function intcode(arr, phaseSetting, input) {
 				}
 			}
 		} else if(opcodeAddress === 8) {
-			//logs the parameter
-			if(opParamTenThou === 2) {
-				if(opcodeValueOne === opcodeValueTwo) {
+			//changes a value in the program to 1 or 0 depending on its parameters values
+			if(paramModeThree === 2) {
+				if(opcodeParamOne === opcodeParamTwo) {
 					arr[relativeBase + arr[i + 3]] = 1;
 					i = i + 4;
 				} else {
@@ -148,7 +135,7 @@ function intcode(arr, phaseSetting, input) {
 					i = i + 4;
 				}
 			} else{
-				if(opcodeValueOne === opcodeValueTwo) {
+				if(opcodeParamOne === opcodeParamTwo) {
 					arr[arr[i + 3]] = 1;
 					i = i + 4;
 				} else {
@@ -157,11 +144,13 @@ function intcode(arr, phaseSetting, input) {
 				}
 			}
 		} else if(opcodeAddress === 0) {
+			//goes to next instruction
 			i = i + 1;
 		} else if(opcodeAddress === 9) {
-			if(opParamHund === 1) {//
+			//modifies the relative base
+			if(paramModeOne === 1) {
 				relativeBase = relativeBase + arr[i + 1];
-			} else if(opParamHund === 2) {
+			} else if(paramModeOne === 2) {
 				relativeBase = relativeBase + arr[relativeBase + arr[i + 1]];
 			} else {
 				relativeBase = relativeBase + arr[arr[i + 1]];
@@ -174,4 +163,4 @@ function intcode(arr, phaseSetting, input) {
 }
 
 puzzleInput = [1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1101,0,3,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,904,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1102,1,39,1013,1102,1,21,1018,1101,0,336,1027,1102,1,38,1012,1101,534,0,1025,1101,539,0,1024,1101,0,380,1023,1102,1,23,1014,1102,29,1,1000,1102,24,1,1019,1102,1,28,1011,1101,339,0,1026,1101,31,0,1005,1102,36,1,1017,1102,26,1,1007,1102,1,407,1028,1101,387,0,1022,1101,0,30,1001,1101,34,0,1010,1102,1,32,1006,1101,0,1,1021,1102,27,1,1008,1102,22,1,1004,1102,1,20,1015,1101,0,37,1016,1101,0,0,1020,1102,1,398,1029,1101,25,0,1009,1101,0,35,1003,1101,33,0,1002,109,27,1206,-6,197,1001,64,1,64,1105,1,199,4,187,1002,64,2,64,109,-22,2107,26,3,63,1005,63,217,4,205,1105,1,221,1001,64,1,64,1002,64,2,64,109,17,21107,40,39,-8,1005,1014,241,1001,64,1,64,1105,1,243,4,227,1002,64,2,64,109,-8,1206,6,261,4,249,1001,64,1,64,1106,0,261,1002,64,2,64,109,-7,2108,24,0,63,1005,63,281,1001,64,1,64,1105,1,283,4,267,1002,64,2,64,109,11,21102,41,1,-3,1008,1015,42,63,1005,63,303,1105,1,309,4,289,1001,64,1,64,1002,64,2,64,109,1,1205,2,327,4,315,1001,64,1,64,1105,1,327,1002,64,2,64,109,10,2106,0,-2,1106,0,345,4,333,1001,64,1,64,1002,64,2,64,109,-15,21102,42,1,3,1008,1017,42,63,1005,63,367,4,351,1105,1,371,1001,64,1,64,1002,64,2,64,109,-1,2105,1,10,1001,64,1,64,1105,1,389,4,377,1002,64,2,64,109,24,2106,0,-9,4,395,1001,64,1,64,1105,1,407,1002,64,2,64,109,-30,1208,-2,32,63,1005,63,427,1001,64,1,64,1106,0,429,4,413,1002,64,2,64,109,2,1201,0,0,63,1008,63,27,63,1005,63,449,1106,0,455,4,435,1001,64,1,64,1002,64,2,64,109,5,21107,43,44,0,1005,1014,473,4,461,1106,0,477,1001,64,1,64,1002,64,2,64,109,-16,1202,3,1,63,1008,63,33,63,1005,63,501,1001,64,1,64,1106,0,503,4,483,1002,64,2,64,109,10,1207,-4,21,63,1005,63,523,1001,64,1,64,1106,0,525,4,509,1002,64,2,64,109,11,2105,1,5,4,531,1106,0,543,1001,64,1,64,1002,64,2,64,109,-8,21101,44,0,5,1008,1016,47,63,1005,63,563,1106,0,569,4,549,1001,64,1,64,1002,64,2,64,109,-13,2102,1,8,63,1008,63,34,63,1005,63,593,1001,64,1,64,1105,1,595,4,575,1002,64,2,64,109,8,1208,-1,31,63,1005,63,617,4,601,1001,64,1,64,1106,0,617,1002,64,2,64,109,-8,2108,33,4,63,1005,63,635,4,623,1105,1,639,1001,64,1,64,1002,64,2,64,109,10,1202,-1,1,63,1008,63,26,63,1005,63,665,4,645,1001,64,1,64,1105,1,665,1002,64,2,64,109,-9,2107,30,1,63,1005,63,685,1001,64,1,64,1105,1,687,4,671,1002,64,2,64,109,25,1205,-4,703,1001,64,1,64,1105,1,705,4,693,1002,64,2,64,109,-19,2101,0,-5,63,1008,63,26,63,1005,63,725,1105,1,731,4,711,1001,64,1,64,1002,64,2,64,109,6,1207,-2,26,63,1005,63,749,4,737,1105,1,753,1001,64,1,64,1002,64,2,64,109,-10,21108,45,46,9,1005,1010,769,1105,1,775,4,759,1001,64,1,64,1002,64,2,64,109,-10,1201,10,0,63,1008,63,30,63,1005,63,801,4,781,1001,64,1,64,1106,0,801,1002,64,2,64,109,21,21108,46,46,3,1005,1015,819,4,807,1106,0,823,1001,64,1,64,1002,64,2,64,109,-4,2102,1,-3,63,1008,63,31,63,1005,63,849,4,829,1001,64,1,64,1106,0,849,1002,64,2,64,109,-5,2101,0,1,63,1008,63,22,63,1005,63,875,4,855,1001,64,1,64,1105,1,875,1002,64,2,64,109,17,21101,47,0,-3,1008,1017,47,63,1005,63,897,4,881,1105,1,901,1001,64,1,64,4,64,99,21101,0,27,1,21102,1,915,0,1105,1,922,21201,1,38480,1,204,1,99,109,3,1207,-2,3,63,1005,63,964,21201,-2,-1,1,21101,0,942,0,1106,0,922,21202,1,1,-1,21201,-2,-3,1,21101,957,0,0,1105,1,922,22201,1,-1,-2,1106,0,968,22101,0,-2,-2,109,-3,2105,1,0];
-console.log(intcode(puzzleInput,2,2));
+console.log(intcodeProgram(puzzleInput,2));
