964693141b78b245ca54adc02256eb5567777b0e
Timothy Beck <barricuda1993@yahoo.com>
2020-12-03 00:34:37 -0700

    too tired to refactor this

--- 2020/12-03-20/challenge-2/script.js
+++ 2020/12-03-20/challenge-2/script.js
@@ -1,3 +1,29 @@
 const fs = require('fs');
-let input = fs.readFileSync('../challenge-1/input.txt');
-console.log(input);
+let input = fs.readFileSync('../challenge-1/input.txt', {encoding:'utf8', flag:'r'});
+
+let test = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#\n..#.#...#.#\n..#.#...#.#";
+
+function solve(input, rise, run,) {
+    input = input.split('\n');
+    let yPos = 0; //input.length;
+    let xPos = 0; //input.length / rise * run;
+    let treeCount = 0;
+    for(yPos;yPos<input.length;yPos = yPos + rise){
+        if(input[yPos][xPos] === "#"){
+            treeCount++;
+        }
+        xPos = (xPos + run) ;
+        xPos > input[0].length-1 ? xPos = (xPos % input[0].length):xPos;
+    }
+    return treeCount;
+}
+
+function solveIt(input) {
+    let slope1 = solve(input,1,1);
+    let slope2 =solve(input,1,3);
+    let slope3 =solve(input,1,5);
+    let slope4 =solve(input,1,7);
+    let slope5 =solve(input,2,1);
+    return slope1*slope2*slope3*slope4*slope5;
+}
+console.log(solveIt(input));
