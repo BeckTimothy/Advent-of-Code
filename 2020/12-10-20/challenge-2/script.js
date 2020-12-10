a69fe43b7df1b9e59fd26d05e401f4b3b3b16e49
Timothy Beck <johndaveleet@gmail.com>
2020-12-10 11:36:40 -0700

    kinda finished day 10 part 2 but theres some wonky issue with the data being corrupted form my number being stronomically large

--- 2020/12-10-20/challenge-2/script.js
+++ 2020/12-10-20/challenge-2/script.js
@@ -1,1 +1,23 @@
 let input = require("fs").readFileSync("../challenge-1/input.txt", { encoding: "utf-8", flag: 'r'}).trim();
+input = input.split(/[\r\n]/);
+let testInput = require("fs").readFileSync("../challenge-1/testInput.txt", { encoding: "utf-8", flag: 'r'}).trim();
+testInput = testInput.split(/[\n]/);
+let testInputTwo = require("fs").readFileSync("../challenge-1/testInputTwo.txt", { encoding: "utf-8", flag: 'r'}).trim();
+testInputTwo = testInputTwo.split(/[\n]/);
+
+
+function solve(input) {
+	input = input.sort( (a, b) => Number(a) - Number(b) );
+	input.unshift('0'); //add the starting value to the array
+	input.push(`${Number(input[input.length-1]) + 3}`); //add the ending value to the array
+	let countArr = [1];
+	let iterate = (index, joltDif) => (input[index - joltDif] >= (input[index] - 3) ) ? countArr[index - joltDif] : 0;
+	for(let i=1;i<input.length;i++){
+		let count = iterate(i, 1) + iterate(i, 2) + iterate(i, 3);
+		countArr.push(count);
+	}
+	return countArr[countArr.length-1].toLocaleString('fullwide', {useGrouping:false});
+}
+
+
+console.log(solve(input));
