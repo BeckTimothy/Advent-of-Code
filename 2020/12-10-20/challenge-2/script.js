6ac5bb2a451b1385b6417ad652b12496673fad7d
Timothy Beck <johndaveleet@gmail.com>
2020-12-10 12:47:44 -0700

    somehow I reverted some changes before last commit.

--- 2020/12-10-20/challenge-2/script.js
+++ 2020/12-10-20/challenge-2/script.js
@@ -1,11 +1,11 @@
 let input = require("fs").readFileSync("../challenge-1/input.txt", { encoding: "utf-8", flag: 'r'}).trim();
-input = input.split(/[\r\n]/);
+input = input.split(/[\n]/);
 
 function solve(input) {
 	input = input.sort( (a, b) => Number(a) - Number(b) );
 	input.unshift('0'); //add the starting value to the array
 	input.push(`${Number(input[input.length-1]) + 3}`); //add the ending value to the array
-	let countArr = [Big(1)];
+	let countArr = [1];
 	let iterate = (index, joltDif) => (input[index - joltDif] >= (input[index] - 3) ) ? Number(countArr[index - joltDif]) : 0;
 	for(let i=1;i<input.length;i++){
 		let count = iterate(i, 1) + iterate(i, 2) + iterate(i, 3);
