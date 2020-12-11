8c3a73c0cf659d8b9905c1087fbc8aca1bd873cd
Timothy Beck <dev@timothybeck.com>
2020-12-11 08:25:42 -0700

    added html pages to chllenge directories, started day 11 part 1

--- 2020/12-11-20/challenge-1/script.js
+++ 2020/12-11-20/challenge-1/script.js
@@ -1,1 +1,64 @@
 let input = require("fs").readFileSync("../challenge-1/input.txt", { encoding: "utf-8", flag: 'r'}).trim();
+input = input.split(/\n/);
+for(let i=0; i < input.length; i++) {
+    input[i] = input[i].split('');
+}
+function solve(input) {
+    let tempArr = input;
+    let n = null;
+    let s = null;
+    let e = null;
+    let w = null;
+    let ne = null;
+    let nw = null;
+    let se = null;
+    let sw = null;
+    for(let i=0; i < input.length; i++) {
+        for(let j = 0; j < input[i].length; j++) {
+
+            (i > 0) ? n = input[i-1][j] : n = null;
+            (i > 0 && j < input[i].length-1) ? ne = input[i-1][j+1] : ne = null;
+            (i > 0 && j > 0) ? nw = input[i-1][j-1] : nw = null;
+            (i < input.length-1) ? s = input[i+1][j] : s = null;
+            (i < input.length-1 && j>0) ? sw = input[i+1][j-1] : sw = null;
+            (i < input.length-1 && j<input[i].length-1) ? se = input[i+1][j+1] : se = null;
+            (j > 0) ? w = input[i][j-1] : w = null;
+            (j < input[i].length-1) ? e = input[i][j+1] : e = null;
+            let rose = [n,s,e,w,se,sw,nw,ne];
+            let sittingCount = 0;
+            let emptyCount = 0;
+            for (let k=0;k<rose.length;k++){
+                if (rose[k] === 'L'){
+                    emptyCount++
+                }else if(rose[k] === '#'){
+                    sittingCount++
+                }
+
+            }
+            if(input[i][j] === 'L' && sittingCount === 0){
+                tempArr[i][j] = '#';
+            }
+            if(input[i][j] === '#' && sittingCount >= 4){
+                tempArr[i][j] = 'L';
+            }
+        }
+    }
+    if(tempArr === input){
+        return tempArr;
+    } else{
+        let answer = solve(tempArr);
+        for(let k=0;k<answer.length;k++){
+            answer[k] = answer[k].join('')
+        }
+        answer = answer.join('');
+        let counter = 0;
+        for(let k=0;k<answer.length;k++){
+            if(answer[k] === '#'){
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
+console.log(solve(input));
--- README.md
+++ README.md
@@ -10,7 +10,10 @@ For more information please see: https://adventofcode.com/2020/about
 
 - [2020 Challenges - 18/50 Completed](https://github.com/BeckTimothy/Advent-of-Code/blob/master/2020/README.md)
 - [2019 Challenges - 18/50 Completed](https://github.com/BeckTimothy/Advent-of-Code/blob/master/2019/README.md)
-
+- 2018 Challenges - 0/50 Completed
+- 2017 Challenges - 0/50 Completed
+- 2016 Challenges - 0/50 Completed
+- 2015 Challenges - 0/50 Completed
 ---
 
 
