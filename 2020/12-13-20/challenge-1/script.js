eaa36e6b1f5b3269d26a5a75e3ddc34b3110199a
Timothy Beck <dev@timothybeck.com>
2020-12-13 11:26:20 -0700

    completed day 13 part 1

--- 2020/12-13-20/challenge-1/script.js
+++ 2020/12-13-20/challenge-1/script.js
@@ -1,1 +1,19 @@
-let input = require("fs").readFileSync("../challenge-1/input.txt", { encoding: "utf-8", flag: 'r'}).trim();
+let input = require("fs").readFileSync("../challenge-1/input.txt", {encoding: "utf-8", flag: 'r'}).trim();
+input = input.split(/\n/);
+
+function solve(input) {
+    input[1] = input[1].split(',');
+    let buses = new Set();
+    input[1].forEach((x) => {
+        buses.add(x)
+    });
+    let arr = [];
+    buses.delete('x');
+    buses.forEach((x) => {
+        arr.push(Number(x))
+    });
+    arr = arr.reduce((x, y) => { return x - (input[0] % x) < y - (input[0] % y) ?x : y});
+    return arr * (arr - (input[0] % arr));
+}
+
+console.log(solve(input));
