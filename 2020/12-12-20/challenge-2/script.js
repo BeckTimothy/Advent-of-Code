c8bae05e822dbc5d8510d86424e717eb8fa6d214
Timothy Beck <johndaveleet@gmail.com>
2020-12-14 13:35:28 -0700

    refactored 12-2 to be more easily readable.

--- 2020/12-12-20/challenge-2/script.js
+++ 2020/12-12-20/challenge-2/script.js
@@ -1,47 +1,45 @@
 let input = require("fs").readFileSync("../challenge-1/input.txt", { encoding: "utf-8", flag: 'r'}).trim();
-input = input.split(/\n/);
-console.log(input);
+input = input.replace(/\r/gi, "").split(/\n/);
 function solve(input) {
-    let wpX = 10;
-    let wpY = 1;
-    let shX = 0;
-    let shY = 0;
+    let waypointX = 10;
+    let waypointY = 1;
+    let shipX = 0;
+    let shipY = 0;
     for (let i = 0; i < input.length; i++) {
         let value = Number(input[i].substr(1));
-        console.log(value);
         switch (input[i].substr(0, 1)) {
             case 'N':
-                wpY = wpY + value;
+                waypointY = waypointY + value;
                 break;
             case 'S':
-                wpY = wpY - value;
+                waypointY = waypointY - value;
                 break;
             case 'E':
-                wpX = wpX + value;
+                waypointX = waypointX + value;
                 break;
             case 'W':
-                wpX = wpX - value;
+                waypointX = waypointX - value;
                 break;
             case 'L':
                 for(let j=0;j<value;j+=90){
-                    let temp = wpX;
-                    wpX = 0 - wpY;
-                    wpY = temp;
+                    let temp = waypointX;
+                    waypointX = 0 - waypointY;
+                    waypointY = temp;
                 }
                 break;
             case 'R':
                 for(let j=0;j<value;j+=90){
-                    let temp = wpY;
-                    wpY = 0 - wpX;
-                    wpX = temp;
+                    let temp = waypointY;
+                    waypointY = 0 - waypointX;
+                    waypointX = temp;
                 }
                 break;
             case 'F':
-                    shX = shX + (wpX * value);
-                    shY = shY + (wpY * value);
+                    shipX = shipX + (waypointX * value);
+                    shipY = shipY + (waypointY * value);
                 break;
         }
     }
-    return Math.abs(shX) + Math.abs(shY);
+    return Math.abs(shipX) + Math.abs(shipY);
 }
 console.log(solve(input));
