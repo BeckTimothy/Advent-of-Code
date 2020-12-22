444581c3e35974a7318c4adf6dcd23153516f551
Timothy Beck <johndaveleet@gmail.com>
2020-12-22 09:07:49 -0700

    added day 17 input

--- 2020/12-17-20/challenge-1/script.js
+++ 2020/12-17-20/challenge-1/script.js
@@ -1,1 +1,7 @@
 let input = require("fs").readFileSync("../challenge-1/input.txt", { encoding: "utf-8", flag: 'r'}).trim();
+//x,y,z
+//if any input is z[0],x[0],y[0], or z[length],x[length],y[length] add items to both ends of every item in that plane
+
+
+
+
