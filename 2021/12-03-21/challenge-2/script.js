008d1bbb6892c2da39a440155e2ca6909b3dce1e
BeckTimothy <johndaveleet@gmail.com>
2021-12-03 03:14:20 -0700

    refactored d3p2 a little

--- 2021/12-03-21/challenge-2/script.js
+++ 2021/12-03-21/challenge-2/script.js
@@ -1,67 +1,38 @@
 const fs = require('fs');
 let input = fs.readFileSync('./input.txt');
 input = input.toString().split('\n');
-let iter = input[0].split('');
-let midVal = Math.floor(input.length / 2);
-
-for(let i=1;i<input.length;i++){
-    let binary = input[i].split('');
-    binary.forEach((num, index) => {iter[index] = Number(iter[index])+Number(num)})
-}
-
-let gammaRate = iter.map((x,i) => {return iter[i]>midVal?1:0}).join('');
-let epsilon = iter.map((x,i) => {return iter[i]>midVal?0:1}).join('');
-console.log(`gammaRate: ${parseInt(gammaRate,2)}`);
-console.log(`epsilon value: ${parseInt(epsilon, 2)}`);
-console.log(`power consumption: ${parseInt(epsilon,2) * parseInt(gammaRate,2)}`);
-
-let inputsToFilter = input;
-let position=0;
-let lookup = null;
-
-while(inputsToFilter.length > 1){
-
-    iter = inputsToFilter[0].split('');
-    midVal = Math.ceil(inputsToFilter.length / 2);
-
-
-    for(let i=1;i<inputsToFilter.length;i++){
-        let binary = inputsToFilter[i].split('');
-        binary.forEach((num, index) => {iter[index] = Number(iter[index])+Number(num)})
+//returns a binary of the most or least common values for each bit
+const commonValues = (arr, type) => {
+    let it = arr[0].split('');
+    let mid = arr.length / 2;
+    for(let i=1;i<arr.length;i++){
+        let binary = arr[i].split('');
+        binary.forEach((num, index) => {it[index] = +it[index]+ +num})
     }
-
-    let mostCommonValue = iter.map((x,i) => {return iter[i]>=midVal?1:0}).join('');
-
-    inputsToFilter = inputsToFilter.filter(n => {
-        lookup = n;
-        return n[position] === mostCommonValue[position]
-    })
-    position++
+    return type==="most"
+        ?it.map((x,i) => {return it[i]>=mid?1:0}).join('')
+        :it.map((x,i) => {return it[i]>=mid?0:1}).join('');
 }
-let oxGenRating = parseInt(inputsToFilter.length===1?inputsToFilter[0]:lookup,2);
+//uses the most or least common values to determine which binaries to filter
+const parseBinaries = (arr, type) => {
+    let i=0;
+    let lastBinary = null;
+    while(arr.length > 1){
+        let commonValue = commonValues(arr,type);
+        arr = arr.filter(binary => {
+            lastBinary = binary;
+            return binary[i] === commonValue[i]
+        })
+        i++
+    }
+    return parseInt(arr.length===1?arr[0]:lastBinary,2);
+}
+
+const getOxygenGeneratorRating = arr => parseBinaries(arr, "most");
+const getCO2ScrubberRating = arr => parseBinaries(arr, "least");
+
+let oxGenRating = getOxygenGeneratorRating(input);
 console.log(`oxGenRating: ${oxGenRating}`);
-
-inputsToFilter = input;
-position=0;
-lookup = null;
-while(inputsToFilter.length > 1){
-
-    iter = inputsToFilter[0].split('');
-    midVal = Math.ceil(inputsToFilter.length / 2);
-
-
-    for(let i=1;i<inputsToFilter.length;i++){
-        let binary = inputsToFilter[i].split('');
-        binary.forEach((num, index) => {iter[index] = Number(iter[index])+Number(num)})
-    }
-
-    let leastCommonValue = iter.map((x,i) => {return iter[i]>=midVal?0:1}).join('');
-    inputsToFilter = inputsToFilter.filter(n => {
-        lookup = n;
-        return n[position] === leastCommonValue[position]
-    })
-    position++
-}
-let scrubRating = parseInt(inputsToFilter.length===1?inputsToFilter[0]:lookup,2);
+let scrubRating = getCO2ScrubberRating(input);
 console.log(`scrubberRating: ${scrubRating}`);
 console.log(`lifeSupportRating: ${scrubRating * oxGenRating}`)
