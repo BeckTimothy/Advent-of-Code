ca957189bdcb1374f5313639b62c97699fd338b0
BeckTimothy <johndaveleet@gmail.com>
2021-12-07 04:18:50 -0700

    docs

--- 2021/12-07-21/challenge-2/script.js
+++ 2021/12-07-21/challenge-2/script.js
@@ -3,12 +3,44 @@ let input = fs.readFileSync('./input.txt');
 input = input.toString().split(',');
 
 input = input.sort((x,y) => {return Number(x) - Number(y)});
-console.log(input)
-console.log(Math.floor(input.length/2))
-const median = (arr) => {return input[Math.floor((input.length/2)) ]}
+
+let total = 0;
+input.forEach(x => {total+=Number(x)})
+let mean = total / input.length;
+let indexOfMean;
+let indexOfMedian = Math.floor((input.length/2));
+const median = (arr) => {return input[indexOfMedian]}
 let arrMedian = median(input)
-let num = 0;
-input.forEach(x => {
-    num += Math.abs(x-arrMedian)
+
+input.forEach((x,i) => {
+    if(Number(x) < Number(arrMedian)){
+        indexOfMean = Number(x) <= Math.floor(mean) ? i : indexOfMean;
+    } else {
+        indexOfMean = Number(x) <= Math.ceil(mean) ? i : indexOfMean;
+    }
 })
-console.log(num)
+
+let bestFuelCost = 9999999999;
+let bestIndex;
+for(let i = input[indexOfMean]+1; i >= input[indexOfMedian]; i--){
+    let count = 0;
+    for(let j=0;j<input.length;j++){
+        let diff = Math.abs(Number(i) - Number(input[j]));
+        if(diff > 0) {
+            let multi = diff + 1;
+            if (diff % 2 === 0) {
+                count += (multi * (diff / 2))
+            } else {
+                count += (((multi * ((diff - 1) / 2))) + (multi / 2))
+            }
+        }
+
+    }
+    if(count < bestFuelCost){
+        bestFuelCost = count;
+        bestIndex = i;
+    }
+}
+
+console.log(bestIndex)
+console.log(bestFuelCost)
