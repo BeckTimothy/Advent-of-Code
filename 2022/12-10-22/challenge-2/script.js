08d9f73744791396a66ff8810a8058fa63ffa235
BeckTimothy <johndaveleet@gmail.com>
2022-12-10 11:02:42 -0700

    renamed variables to be more readable

--- 2022/12-10-22/challenge-2/script.js
+++ 2022/12-10-22/challenge-2/script.js
@@ -2,39 +2,29 @@ const fs = require('fs');
 let input = fs.readFileSync('./input.txt');
 input = input.toString().trim().split('\r\n');
 
-let X = 1;
-let awaitVal = 0;
-let operation = 0
-let offset = 0;
-let trackedCycles = [[],[],[],[],[],[]];
+let X = 1, awaitVal = 0, operation = 0, offset = 0;
+let CRT = [[],[],[],[],[],[]];
+CRT.forEach(x=>{for(let i=0;i<40;i++){x.push('░')}});
 
-trackedCycles.forEach(x=>{
-    for(let i=0;i<40;i++){x.push('.')}
-})
-const trackCycle = (cycleNum) => {
+const trackSprite = (cycleNum) => {
     let row = Math.ceil(cycleNum/40)-1
     let pixel = (cycleNum % 40)-1
-    if(Math.abs( pixel - X) <= 1){trackedCycles[row][pixel] = '#'}
+    if(Math.abs( pixel - X) <= 1){CRT[row][pixel] = '█'}
 }
+
 for(let i=1;i-offset<input.length;i++) {
     if(awaitVal === 0){
-        switch(input[(i-1)-offset].substring(0,4)){
-            case 'noop':
-                break;
-            case 'addx':
-                //console.log(Number(input[(i-1)-offset].split(' ')[1]))
-                operation = Number(input[(i-1)-offset].split(' ')[1]);
-                awaitVal=2;
-                break;
+        if(input[(i-1)-offset].substring(0,4) === 'addx'){
+            operation = Number(input[(i-1)-offset].split(' ')[1]);
+            awaitVal=2;
         }
     }
-    trackCycle(i);
-
+    trackSprite(i);
     if(awaitVal === 1) {
         offset++;
         X+=operation;
     }
     awaitVal>0?awaitVal--:awaitVal=0;
 }
-trackedCycles = trackedCycles.map(line=>line.join(''))
-console.log(trackedCycles)
+CRT = CRT.map(line=>line.join(''))
+console.log(CRT)
